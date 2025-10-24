635132175e37b5517eab587e2d3f1bee5d2d9360
giovannafg <giovannafelp@gmail.com>
2025-10-24 19:40:51 -0300

    feat: Adiciona gráfico de pizza na tela de dashboards

---
+++ web/app/dashboards/page.tsx
@@ -0,0 +1,77 @@
+'use client'; 
+
+import { useState, useEffect } from 'react';
+import Navbar from "../components/navbar/navbar";
+import GraficoPizza from "../components/graficoPizza/graficoPizza"; 
+import { Merriweather } from "next/font/google";
+
+const merriweather = Merriweather({
+  subsets: ["latin"],
+  weight: ["700"],
+});
+
+interface CrimeData {
+  id: string;
+  crimeGenre: string;
+  crimeType: string;
+  description: string;
+  location: string;
+  crimeDate: string;
+  reporterDetails: {
+    ageGroup: string;
+    ethnicity: string;
+    genderIdentity: string;
+    sexualOrientation: string;
+  };
+  createdDate: string;
+  resolved: boolean;
+  partitionKey: string;
+  _rid: string;
+  _self: string;
+  _etag: string;
+  _attachments: string;
+  _ts: number;
+}
+
+
+export default function DashboardPage() {
+  const [apiData, setApiData] = useState<CrimeData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/reports'); 
+        
+        if (!response.ok) {
+          throw new Error('Falha ao buscar dados da API');
+        }
+        
+        const data = await response.json();
+        setApiData(data); 
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    };
+
+    fetchData();
+  }, []); 
+
+  return (
+    <>
+      <Navbar />
+      <main className="min-h-screen flex flex-col items-center bg-neutral-900 text-white">
+        <section className="w-full max-w-[1920px] px-[128px] py-16 flex flex-col md:flex-row items-start justify-between gap-12">
+          <div className="md:w-1/2 mt-3">
+            <h1 className={`${merriweather.className} text-4xl font-bold mb-3`}>
+              Dashboards
+            </h1>
+            <div className="w-8 h-[3px] bg-cyan-500 mb-5"></div>
+          </div>
+        </section>
+
+        <GraficoPizza data={apiData} />
+        
+      </main>
+    </>
+  );
+}
